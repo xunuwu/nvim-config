e10f6a82c2e1bce50f2eb9aa28c961e70c814413
xunuwu <xunuwu@gmail.com>
2025-05-28 04:49:00 +0200

    remove elixirls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,13 +5,10 @@ return {
 			root_markers = { ".git", ".jj" },
 		})
 
-		vim.lsp.config("elixirls", { cmd = "elixir-ls" })
-
 		vim.lsp.enable("lua_ls")
 		vim.lsp.enable("nil_ls")
 		vim.lsp.enable("nixd")
 		vim.lsp.enable("clangd")
-		vim.lsp.enable("elixirls")
 		vim.lsp.enable("gopls")
 		vim.lsp.enable("zls")
 		vim.lsp.enable("cssls")
