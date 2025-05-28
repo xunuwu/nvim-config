b8059726da9412b15b0c3c70c68447b7679a8b2b
xunuwu <xunuwu@gmail.com>
2025-05-28 20:53:51 +0200

    only call vim.lsp.enable once with a list

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,12 +5,14 @@ return {
 			root_markers = { ".git", ".jj" },
 		})
 
-		vim.lsp.enable("lua_ls")
-		vim.lsp.enable("nixd")
-		vim.lsp.enable("clangd")
-		vim.lsp.enable("gopls")
-		vim.lsp.enable("zls")
-		vim.lsp.enable("cssls")
-		vim.lsp.enable("vtsls")
+		vim.lsp.enable({
+			"lua_ls",
+			"nixd",
+			"clangd",
+			"gopls",
+			"zls",
+			"cssls",
+			"vtsls",
+		})
 	end,
 }
