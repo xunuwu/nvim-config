f2127a5caa6aba90b8dc3cf2a4d7ea2af9a73580
xunuwu <xunuwu@gmail.com>
2025-05-28 23:35:46 +0200

    treesitter textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,18 +1,38 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	dependencies = "nvim-treesitter/nvim-treesitter-textobjects",
 	build = ":TSUpdate",
 	event = { "BufReadPost", "BufNewFile" },
 	main = "nvim-treesitter.configs",
 	opts = {
 		highlight = { enable = true },
 		indent = { enable = true },
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "gnn", -- set to `false` to disable one of the mappings
-				node_incremental = "grn",
-				scope_incremental = "grc",
-				node_decremental = "grm",
+		textobjects = {
+			select = {
+				enable = true,
+				lookahead = true,
+				keymaps = {
+					["af"] = { query = "@function.outer", desc = "ts function outer" },
+					["if"] = { query = "@function.inner", desc = "ts function inner" },
+					["ab"] = { query = "@block.outer", desc = "ts block outer" },
+					["ib"] = { query = "@block.inner", desc = "ts block inner" },
+				},
+			},
+			move = {
+				enable = true,
+				set_jumps = true,
+				goto_next_start = {
+					["]m"] = "@function.outer",
+				},
+				goto_next_end = {
+					["]M"] = "@function.outer",
+				},
+				goto_previous_start = {
+					["[m"] = "@function.outer",
+				},
+				goto_previous_end = {
+					["[M"] = "@function.outer",
+				},
 			},
 		},
 	},
